c1199f16fa44c74e9205597827e2b9cc2aef45df
Gray Tempest <101955396+GrayTempest-400@users.noreply.github.com>
2023-04-09 16:53:23 +0800

    Add files via upload

--- warthunder_findthank.py
+++ warthunder_findthank.py
@@ -1,22 +1,42 @@
-#请注意，您需要将上面的代码中的“tank_classifier.xml”替换为您自己训练的坦克分类器的名称。此外，您还需要将“test_image.jpg”替换为要检测的图像的名称。
+#请注意，您需要将上面的代码中的“tank_classifier.xml”替换为您自己训练的坦克分类器的名称
 import cv2
+import numpy as np
 
-# Load the cascade
-tank_cascade = cv2.CascadeClassifier('tank_classifier.xml')
+# 获取屏幕分辨率
+screen_size = (1920, 1080)
 
-# Read the input image
-img = cv2.imread('input_image.jpg')
+# 创建VideoCapture对象
+cap = cv2.VideoCapture(0)
 
-# Convert into grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+while True:
+    # 读取帧
+    ret, frame = cap.read()
 
-# Detect tanks
-tanks = tank_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    # 将帧调整为屏幕大小
+    frame = cv2.resize(frame, screen_size)
 
-# Draw rectangles around the tanks
-for (x, y, w, h) in tanks:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    # 在帧上运行分类器
+    # 这里假设你已经训练好了一个名为tank_classifier.xml的分类器
+    # 如果没有，请参考OpenCV文档中的教程来训练一个分类器
+    classifier = cv2.CascadeClassifier('tank_classifier.xml')
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    tanks = classifier.detectMultiScale(gray)
 
-# Display the output
-cv2.imshow('img', img)
-cv2.waitKey()
+    # 在帧上框选出分类器检测到的内容
+    for (x, y, w, h) in tanks:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # 显示帧
+    cv2.imshow('frame', frame)
+
+    # 按'q'键退出循环
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# 释放VideoCapture对象并关闭所有窗口
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
