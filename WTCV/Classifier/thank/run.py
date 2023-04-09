6eaa436855d977c44690fcd4ce6de352c21daf36
Gray Tempest <101955396+GrayTempest-400@users.noreply.github.com>
2023-04-09 09:57:30 +0800

    Add files via upload

---
+++ WTCV/Classifier/thank/run.py
@@ -0,0 +1,26 @@
+import os
+import cv2
+import pandas as pd
+
+path = 'no_t72'  # 坦克数据集正样本的路径
+filelist = os.listdir(path)
+count = 1000  # 开始文件名1000.jpg
+for file in filelist:
+    Olddir = os.path.join(path, file)
+    if os.path.isdir(Olddir):
+        continue
+    filename = os.path.splitext(file)[0]
+    filetype = os.path.splitext(file)[1]
+    Newdir = os.path.join(path, str(count) + filetype)
+    os.rename(Olddir, Newdir)
+    count += 1
+
+
+
+for n in range(10000, 11790):  # 代表正数据集中开始和结束照片的数字
+    path = 'no_t72' + str(n) + '.jpg'
+    # 读取图片
+    img = cv2.imread(path)
+    img = cv2.resize(img, (80, 80))  # 修改样本像素为20x20
+    cv2.imwrite('on_t72' + str(n) + '.jpg', img)
+    n += 1
