6d9fd2fbbe42015147abc4465e2e3fc181bf14ab
Gray Tempest <101955396+GrayTempest-400@users.noreply.github.com>
2023-04-09 20:28:47 +0800

    Add files via upload

--- warthunder_findthank.py
+++ warthunder_findthank.py
@@ -1,42 +1,40 @@
-#请注意，您需要将上面的代码中的“tank_classifier.xml”替换为您自己训练的坦克分类器的名称
+#您需要将“recognize”函数中的代码替换为使用cv2自己训练的名为thank.xlm的训练集识别
+import win32gui
+import win32ui
+import win32con
+import win32api
 import cv2
-import numpy as np
 
-# 获取屏幕分辨率
-screen_size = (1920, 1080)
 
-# 创建VideoCapture对象
-cap = cv2.VideoCapture(0)
+def get_window_info():
+    # 获取窗口句柄
+    hwnd = win32gui.FindWindow(None, '窗口标题')
+    # 获取窗口位置信息
+    left, top, right, bottom = win32gui.GetWindowRect(hwnd)
+    width = right - left
+    height = bottom - top
+    # 获取设备上下文DC（Divice Context）
+    hwndDC = win32gui.GetWindowDC(hwnd)
+    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
+    saveDC = mfcDC.CreateCompatibleDC()
+    # 创建位图对象
+    saveBitMap = win32ui.CreateBitmap()
+    saveBitMap.CreateCompatibleBitmap(mfcDC, width, height)
+    saveDC.SelectObject(saveBitMap)
+    # 截图至位图对象
+    saveDC.BitBlt((0, 0), (width, height), mfcDC, (0, 0), win32con.SRCCOPY)
+    # 将位图对象保存至文件
+    saveBitMap.SaveBitmapFile(saveDC, 'screenshot.bmp')
 
-while True:
-    # 读取帧
-    ret, frame = cap.read()
 
-    # 将帧调整为屏幕大小
-    frame = cv2.resize(frame, screen_size)
+def recognize():
+    img = cv2.imread('screenshot.bmp')
+    # 使用cv2自己训练的名为thank.xlm的训练集识别
 
-    # 在帧上运行分类器
-    # 这里假设你已经训练好了一个名为tank_classifier.xml的分类器
-    # 如果没有，请参考OpenCV文档中的教程来训练一个分类器
-    classifier = cv2.CascadeClassifier('tank_classifier.xml')
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    tanks = classifier.detectMultiScale(gray)
 
-    # 在帧上框选出分类器检测到的内容
-    for (x, y, w, h) in tanks:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    # 显示帧
-    cv2.imshow('frame', frame)
-
-    # 按'q'键退出循环
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# 释放VideoCapture对象并关闭所有窗口
-cap.release()
-cv2.destroyAllWindows()
-
+if __name__ == '__main__':
+    get_window_info()
+    recognize()
 
 
 
