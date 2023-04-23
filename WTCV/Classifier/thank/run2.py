8f5669a2ef557533936e57b775d4cb746621cee8
Gray Tempest <101955396+GrayTempest-400@users.noreply.github.com>
2023-04-23 21:59:34 +0800

    Delete WTCV/Classifier/thank directory

--- WTCV/Classifier/thank/run2.py
+++
@@ -1,7 +0,0 @@
-Houzhui=r" 1 0 0 20 20" #后缀
-filelist = open('have_thank.txt','r+',encoding = 'utf-8')
-line = filelist.readlines()
-for file in line:
-    file=file.strip('\n')+Houzhui+'\n'
-    print(file)
-    filelist.write(file)
